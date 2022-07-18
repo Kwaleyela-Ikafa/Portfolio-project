afdc3e3608b578b6be4953f50227cee8f2468524
kwaleyela-ikafa <kwaleyelamusil@gmail.com>
2022-07-18 22:08:34 +0200

    Add menu functionality

--- script.js
+++ script.js
@@ -1,26 +1,11 @@
 let hamburger = document.querySelector(".hamburger");
 
-hamburger.addEventListener("click", function() {
-    document.getElementById("myDropdown").classList.toggle("show");
-})
+hamburger.addEventListener("click", function () {
+  document.getElementById("myDropdown").classList.toggle("show");
+});
 
+let HamBurger = document.querySelector(".ham-container");
 
-
-
-// function myFunction() {
-//     document.getElementById("#myDropdown").classList.toggle("show");
-//   }
-  
-//   // Close the dropdown if the user clicks outside of it
-//   window.onclick = function(event) {
-//     if (!event.target.matches('.hamburger')) {
-//       var dropdowns = document.getElementsByClassName("dropdown-content");
-//       var i;
-//       for (i = 0; i < dropdowns.length; i++) {
-//         var openDropdown = dropdowns[i];
-//         if (openDropdown.classList.contains('show')) {
-//           openDropdown.classList.remove('show');
-//         }
-//       }
-//     }
-//   }
+HamBurger.addEventListener("click", function () {
+  document.getElementById("myHamburger").classList.toggle("change");
+});
