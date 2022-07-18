f2e10149fc79df9519c1c10a719235644dcdf18a
kwaleyela-ikafa <kwaleyelamusil@gmail.com>
2022-07-18 16:12:07 +0200

    Add Dropdown menu

---
+++ script.js
@@ -0,0 +1,26 @@
+let hamburger = document.querySelector(".hamburger");
+
+hamburger.addEventListener("click", function() {
+    document.getElementById("myDropdown").classList.toggle("show");
+})
+
+
+
+
+// function myFunction() {
+//     document.getElementById("#myDropdown").classList.toggle("show");
+//   }
+  
+//   // Close the dropdown if the user clicks outside of it
+//   window.onclick = function(event) {
+//     if (!event.target.matches('.hamburger')) {
+//       var dropdowns = document.getElementsByClassName("dropdown-content");
+//       var i;
+//       for (i = 0; i < dropdowns.length; i++) {
+//         var openDropdown = dropdowns[i];
+//         if (openDropdown.classList.contains('show')) {
+//           openDropdown.classList.remove('show');
+//         }
+//       }
+//     }
+//   }
