4d72a3bde6b61635bf0fef8ae29f93e20cbeb6e5
kwaleyela-ikafa <kwaleyelamusil@gmail.com>
2022-07-18 22:17:16 +0200

    Fix linter errors

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
-let hamburger = document.querySelector(".hamburger");
+const hamburger = document.querySelector('.hamburger');
 
-hamburger.addEventListener("click", function () {
-  document.getElementById("myDropdown").classList.toggle("show");
+hamburger.addEventListener('click', function () {
+  document.getElementById('myDropdown').classList.toggle('show');
 });
 
-let HamBurger = document.querySelector(".ham-container");
+const HamBurger = document.querySelector('.ham-container');
 
-HamBurger.addEventListener("click", function () {
-  document.getElementById("myHamburger").classList.toggle("change");
+HamBurger.addEventListener('click', function () {
+  document.getElementById('myHamburger').classList.toggle('change');
 });
