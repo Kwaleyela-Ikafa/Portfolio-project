fccf5311298c865571082471d192c2746f336926
kwaleyela-ikafa <kwaleyelamusil@gmail.com>
2022-07-19 08:57:01 +0200

    fix linter error

--- script.js
+++ script.js
@@ -1,14 +1,13 @@
-const hamburger = document.querySelector(".hamburger");
-const HamBurger = document.querySelector(".ham-container");
+const hamburger = document.querySelector('.hamburger');
+const HamBurger = document.querySelector('.ham-container');
 
-hamburger.addEventListener("click", () => {
-  document.getElementById("myDropdown").classList.toggle("show");
-  document.getElementById("myHamburger").classList.toggle("change");
+hamburger.addEventListener('click', () => {
+  document.getElementById('myDropdown').classList.toggle('show');
+  document.getElementById('myHamburger').classList.toggle('change');
 });
 
-document.querySelectorAll(".nav-link").forEach((n) =>
-  n.addEventListener("click", () => {
-    document.getElementById("myDropdown").classList.remove("show");
-    document.getElementById("myHamburger").classList.remove("change");
+document.querySelectorAll('.nav-link').forEach((n) => n.addEventListener('click', () => {
+    document.getElementById('myDropdown').classList.remove('show');
+    document.getElementById('myHamburger').classList.remove('change');
   })
 );
