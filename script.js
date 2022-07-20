0d259f0c0a92614051decf41a04295b1a898fbc8
kwaleyela-ikafa <kwaleyelamusil@gmail.com>
2022-07-20 17:39:14 +0200

    Fix linter error

--- script.js
+++ script.js
@@ -20,7 +20,7 @@ const modalInformation = {
   button2: 'See Source <img src="./images/Icons/IconGitHub.png">',
   li: 'html',
   li2: 'Bootstrap',
-  li3: 'Ruby on rails'
+  li3: 'Ruby on rails',
 }
 
 let div1 = document.createElement('div');
@@ -28,21 +28,21 @@ div1.id = 'myModal';
 div1.className = 'modal';
 document.body.appendChild(div1);
 
-const div = document.createElement('div')
+const div = document.createElement('div');
 div.className = 'modal-content';
 div1.appendChild(div);
 
-const div2 = document.createElement('div')
+const div2 = document.createElement('div');
 div2.className = 'modal-header';
 div.appendChild(div2);
 
 const h2 = document.createElement('h2');
-h2.className = 'modal-h2'
+h2.className = 'modal-h2';
 h2.textContent = modalInformation.title;
 div2.appendChild(h2);
 
 const h22 = document.createElement('h2');
-h22.className = 'modal-h22'
+h22.className = 'modal-h22';
 h22.textContent = modalInformation.title2;
 div2.appendChild(h22);
 
@@ -60,18 +60,18 @@ ul.className = 'ul-tags';
 div3.appendChild(ul);
 
 const li = document.createElement('li');
-li.className = 'tags1'
-li.innerHTML = modalInformation.li
+li.className = 'tags1';
+li.innerHTML = modalInformation.li;
 ul.appendChild(li);
 
 const li2 = document.createElement('li');
-li2.className = 'tags2'
-li2.innerHTML = modalInformation.li2
+li2.className = 'tags2';
+li2.innerHTML = modalInformation.li2;
 ul.appendChild(li2);
 
 const li3 = document.createElement('li');
-li3.className = 'tags3'
-li3.innerHTML = modalInformation.li3
+li3.className = 'tags3';
+li3.innerHTML = modalInformation.li3;
 ul.appendChild(li3);
 
 const div4 = document.createElement('div');
@@ -79,43 +79,43 @@ div4.className = 'modal-body';
 div.appendChild(div4);
 
 const img = document.createElement('img');
-img.className = 'modal-image'
+img.className = 'modal-image';
 img.src = modalInformation.imageSrc;
-div4.appendChild(img)
+div4.appendChild(img);
 
 const p = document.createElement('p');
 p.className = 'modal-p';
 p.textContent = modalInformation.description;
-div4.appendChild(p)
+div4.appendChild(p);
 
 const button = document.createElement('button');
 button.className = 'modal-button';
 button.innerHTML = modalInformation.button1;
-div4.appendChild(button)
+div4.appendChild(button);
 
 const button2 = document.createElement('button');
 button2.className = 'modal-button2';
 button2.innerHTML = modalInformation.button2;
-div4.appendChild(button2)
+div4.appendChild(button2);
 
 // Get the modal
-div1 = document.getElementById("myModal");
-const btn = document.getElementById("myBtn");
-span = document.getElementsByClassName("close")[0];
+div1 = document.getElementById('myModal');
+const btn = document.getElementById('myBtn');
+span = document.getElementsByClassName('close')[0];
 
 // When the user clicks the button, open the modal 
-btn.onclick = function() {
-  div1.style.display = "block";
+btn.onclick = () => {
+  div1.style.display = 'block';
 }
 
 // When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  div1.style.display = "none";
+span.onclick = () => {
+  div1.style.display = 'none';
 }
 
 // When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
+window.onclick = function (event) {
   if (event.target === div1) {
-    div1.style.display = "none";
+    div1.style.display = 'none';
   }
 }
